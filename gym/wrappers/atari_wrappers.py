bfd4e8dfd9232ae7c1a3b3c74395978794fcc043
Vashisht Madhavan <vashisht@uber.com>
2018-08-23 13:08:18 -0700

    changing interpolation to bilinear

--- gym/wrappers/atari_wrappers.py
+++ gym/wrappers/atari_wrappers.py
@@ -140,7 +140,7 @@ class WarpFrame(gym.ObservationWrapper):
 
     def observation(self, frame):
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        frame = cv2.resize(frame, (self.width, self.height), interpolation=cv2.INTER_AREA)
+        frame = cv2.resize(frame, (self.width, self.height), interpolation=cv2.INTER_LINEAR)
         return frame[:, :, None]
 
 class FrameStack(gym.Wrapper):
