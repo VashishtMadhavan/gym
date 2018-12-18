6b8027f390e4c2240b703491829aaad4595d81e9
Vashisht Madhavan <vashisht@uber.com>
2018-12-17 20:27:19 -0800

    changing deep sea env to match example

--- gym/envs/toy_text/deep_sea.py
+++ gym/envs/toy_text/deep_sea.py
@@ -3,60 +3,73 @@ from gym import spaces
 from gym.utils import seeding
 import numpy as np
 
+
 class DeepSeaEnv(gym.Env):
-    """
-    Deep Sea Environment from RPF Paper
-    """
     def __init__(self, n=20, random=True):
-        self._n = n
-        self._move_cost = 0.01 / n
+        self._size = size
+        self._move_cost = 0.01 / size
         self._goal_reward = 1.
 
-        self._x = 0
-        self._y = 0
-        self.seed()
+        self._column = 0
+        self._row = 0
 
-        self._act_map = self.np_random.binomial(1, 0.5, n)
+        if randomize:
+          rng = np.random.RandomState(seed)
+          self._action_mapping = rng.binomial(1, 0.5, size)
+        else:
+          self._action_mapping = np.ones(size)
+
         self._reset_next_step = False
-        self.action_space = spaces.Discrete(2)
-        self.observation_space = spaces.Discrete(n**2)
-
-    def seed(self, seed=None):
-        self.np_random, seed = seeding.np_random(seed)
-        return [seed]
 
     def step(self, action):
-        action_right = action == self._act_map[self._x]
-        done = False
+        if self._reset_next_step:
+            return self.reset()
+        # Remap actions according to column (action_right = go right)
+        action_right = action == self._action_mapping[self._column]
+
+        # Compute the reward
+        reward = 0.
+        if self._column == self._size-1 and action_right:
+            reward += self._goal_reward
 
         # State dynamics
         if action_right:  # right
-            self._y += 1
-            reward = -self._move_cost
+            self._column = np.clip(self._column + 1, 0, self._size-1)
+            reward -= self._move_cost
         else:  # left
-            self._y = np.max(self._y - 1, 0)
-            reward = 0
-        self._x += 1
+            self._column = np.clip(self._column - 1, 0, self._size-1)
 
-        if self._y == (self._n - 1):
-            reward += 1
-
-        # Check Termination
-        if self._x == (self._n - 1):
-            done = True
-        return self._get_observation(self._x, self._y), reward, done, {}
-
-    def _get_observation(self, x, y):
-        state_map = np.zeros((self._n, self._n), dtype=np.float32)
-        state_map[x][y] = 1.
-        return state_map.flatten()
+        # Compute the observation
+        self._row += 1
+        if self._row == self._size:
+            observation = self._get_observation(self._row-1, self._column)
+            self._reset_next_step = True
+            return observation, reward, False, {}
+        else:
+            observation = self._get_observation(self._row, self._column)
+            return observation, reward, True, {}
 
     def reset(self):
-        self._x = 0
-        self._y = 0
-        return self._get_observation(self._x, self._y)
+        self._reset_next_step = False
+        self._column = 0
+        self._row = 0
+        observation = self._get_observation(self._row, self._column)
+        return observation, reward, True, {}
+
+    def _get_observation(self, row, column):
+        observation = np.zeros(shape=(self._size, self._size), dtype=np.float32)
+        observation[row, column] = 1
+        return observation
+
+    @property
+    def obs_shape(self):
+        return self.reset().observation.shape
+
+    @property
+    def num_actions(self):
+        return 2
 
     @property
     def optimal_return(self):
-        return self._goal_reward - ((self._n - 1) * self._move_cost)
+        return self._goal_reward - self._move_cost
     
