fb60960aea81d7010215e7b15e62725f6288e4b8
Vashisht Madhavan <vashisht@uber.com>
2019-06-12 13:44:16 -0700

    making maze env a bit easier

--- gym/envs/mujoco/maze_env_utils.py
+++ gym/envs/mujoco/maze_env_utils.py
@@ -1,5 +1,5 @@
-import matplotlib as mpl
-mpl.use('Agg')
+#import matplotlib as mpl
+#mpl.use('Agg')
 import matplotlib.pyplot as plt
 import os.path as osp
 import numpy as np
@@ -101,8 +101,8 @@ def construct_maze(maze_id=0, length=1):
             raise NotImplementedError("Maze_id 0 only has length 1!")
         structure = [
             [1, 1, 1, 1, 1],
-            [1, 'r', 0, 0, 1],
-            [1, 1, 1, 0, 1],
+            [1, 0, 0, 0, 1],
+            [1, 1, 1, 'r', 1],
             [1, 'g', 0, 0, 1],
             [1, 1, 1, 1, 1],
         ]
@@ -192,8 +192,8 @@ def construct_maze(maze_id=0, length=1):
 
 
 def plot_ray(self, reading, ray_idx, color='r'):
-    structure = self.MAZE_STRUCTURE
-    size_scaling = self.MAZE_SIZE_SCALING
+    structure = self.unwrapped.MAZE_STRUCTURE
+    size_scaling = self.unwrapped.MAZE_SIZE_SCALING
     # duplicate cells to plot the maze
     structure_plot = np.zeros(((len(structure) - 1) * 2, (len(structure[0]) - 1) * 2))
     for i in range(len(structure)):
@@ -231,11 +231,11 @@ def plot_ray(self, reading, ray_idx, color='r'):
 
     ax.xaxis.set(ticks=2 * np.arange(len(x_labels)), ticklabels=x_labels)
     ax.yaxis.set(ticks=2 * np.arange(len(y_labels)), ticklabels=y_labels)
-    robot_xy = np.array(self.wrapped_env.get_body_com("torso")[:2])  # the coordinates of this are wrt the init!!
-    ori = self.get_ori()  # for Ant this is computed with atan2, which gives [-pi, pi]
+    robot_xy = np.array(self.unwrapped.get_body_com("torso")[:2])  # the coordinates of this are wrt the init!!
+    ori = self.unwrapped.get_ori()  # for Ant this is computed with atan2, which gives [-pi, pi]
 
     # compute origin cell i_o, j_o coordinates and center of it x_o, y_o (with 0,0 in the top-right corner of struc)
-    o_xy = np.array(self._find_robot())  # this is self.init_torso_x, self.init_torso_y !!: center of the cell xy!
+    o_xy = np.array(self.unwrapped._find_robot())  # this is self.unwrapped.init_torso_x, self.unwrapped.init_torso_y !!: center of the cell xy!
     o_ij = (o_xy / size_scaling).astype(int)  # this is the position in the grid (check if correct..)
 
     o_xy_plot = o_xy / size_scaling * 2
@@ -243,9 +243,9 @@ def plot_ray(self, reading, ray_idx, color='r'):
 
     plt.scatter(*robot_xy_plot)
 
-    # for ray_idx in range(self._n_bins):
-    length_wall = self._sensor_range - reading * self._sensor_range if reading else 1e-6
-    ray_ori = ori - self._sensor_span * 0.5 + ray_idx / (self._n_bins - 1) * self._sensor_span
+    # for ray_idx in range(self.unwrapped._n_bins):
+    length_wall = self.unwrapped._sensor_range - reading * self.unwrapped._sensor_range if reading else 1e-6
+    ray_ori = ori - self.unwrapped._sensor_span * 0.5 + ray_idx / (self.unwrapped._n_bins - 1) * self.unwrapped._sensor_span
     if ray_ori > math.pi:
         ray_ori -= 2 * math.pi
     elif ray_ori < - math.pi:
@@ -261,12 +261,9 @@ def plot_ray(self, reading, ray_idx, color='r'):
     # plt.close()
 
 
-def plot_state(self, name='sensors', state=None):
-    if state:
-        self.wrapped_env.reset(state)
-
-    structure = self.__class__.MAZE_STRUCTURE
-    size_scaling = self.__class__.MAZE_SIZE_SCALING
+def plot_state(self, name='sensors'):
+    structure = self.unwrapped.MAZE_STRUCTURE
+    size_scaling = self.unwrapped.MAZE_SIZE_SCALING
     # duplicate cells to plot the maze
     structure_plot = np.zeros(((len(structure) - 1) * 2, (len(structure[0]) - 1) * 2))
     for i in range(len(structure)):
@@ -305,13 +302,13 @@ def plot_state(self, name='sensors', state=None):
     ax.xaxis.set(ticks=2 * np.arange(len(x_labels)), ticklabels=x_labels)
     ax.yaxis.set(ticks=2 * np.arange(len(y_labels)), ticklabels=y_labels)
 
-    obs = self.get_current_maze_obs()
+    obs = self.unwrapped._get_maze_obs()
 
-    robot_xy = np.array(self.wrapped_env.get_body_com("torso")[:2])  # the coordinates of this are wrt the init
-    ori = self.get_ori()  # for Ant this is computed with atan2, which gives [-pi, pi]
+    robot_xy = np.array(self.unwrapped.get_body_com("torso")[:2])  # the coordinates of this are wrt the init
+    ori = self.unwrapped.get_ori()  # for Ant this is computed with atan2, which gives [-pi, pi]
 
     # compute origin cell i_o, j_o coordinates and center of it x_o, y_o (with 0,0 in the top-right corner of struc)
-    o_xy = np.array(self._find_robot())  # this is self.init_torso_x, self.init_torso_y: center of the cell xy!
+    o_xy = np.array(self.unwrapped._find_robot())  # this is self.unwrapped.init_torso_x, self.unwrapped.init_torso_y: center of the cell xy!
     o_ij = (o_xy / size_scaling).astype(int)  # this is the position in the grid
 
     o_xy_plot = o_xy / size_scaling * 2
@@ -319,9 +316,9 @@ def plot_state(self, name='sensors', state=None):
 
     plt.scatter(*robot_xy_plot)
 
-    for ray_idx in range(self._n_bins):
-        length_wall = self._sensor_range - obs[ray_idx] * self._sensor_range if obs[ray_idx] else 1e-6
-        ray_ori = ori - self._sensor_span * 0.5 + ray_idx / (self._n_bins - 1) * self._sensor_span
+    for ray_idx in range(self.unwrapped._n_bins):
+        length_wall = self.unwrapped._sensor_range - obs[ray_idx] * self.unwrapped._sensor_range if obs[ray_idx] else 1e-6
+        ray_ori = ori - self.unwrapped._sensor_span * 0.5 + ray_idx / (self.unwrapped._n_bins - 1) * self.unwrapped._sensor_span
         if ray_ori > math.pi:
             ray_ori -= 2 * math.pi
         elif ray_ori < - math.pi:
@@ -331,12 +328,13 @@ def plot_state(self, name='sensors', state=None):
         end_xy_plot = (o_ij + end_xy / size_scaling) * 2
         plt.plot([robot_xy_plot[0], end_xy_plot[0]], [robot_xy_plot[1], end_xy_plot[1]], 'r')
 
-        length_goal = self._sensor_range - obs[ray_idx + self._n_bins] * self._sensor_range if obs[
-            ray_idx + self._n_bins] else 1e-6
-        ray_ori = ori - self._sensor_span * 0.5 + ray_idx / (self._n_bins - 1) * self._sensor_span
+        length_goal = self.unwrapped._sensor_range - obs[ray_idx + self.unwrapped._n_bins] * self.unwrapped._sensor_range if obs[
+            ray_idx + self.unwrapped._n_bins] else 1e-6
+        ray_ori = ori - self.unwrapped._sensor_span * 0.5 + ray_idx / (self.unwrapped._n_bins - 1) * self.unwrapped._sensor_span
         # find the end point goal
         end_xy = (robot_xy + length_goal * np.array([math.cos(ray_ori), math.sin(ray_ori)]))
         end_xy_plot = (o_ij + end_xy / size_scaling) * 2
         plt.plot([robot_xy_plot[0], end_xy_plot[0]], [robot_xy_plot[1], end_xy_plot[1]], 'g')
     ax.set_title('sensors: ' + name)
-    plt.close()
+    plt.show()
+    #plt.close()
